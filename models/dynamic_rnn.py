0b5876774dac3033a4caa517abbd51a985658fab
liuzqt <liuzq12@hotmail.com>
2017-06-14 14:32:30 +0800

    fix

--- models/dynamic_rnn.py
+++ models/dynamic_rnn.py
@@ -62,67 +62,67 @@ class DRNN(object):
             biasesClasses = tf.get_variable(name='biasesClasses',
                                             initializer=tf.truncated_normal(
                                                 [config.num_classes]))
+        if is_train:
+            flatten_logits = tf.matmul(flatten_outputs,
+                                       weightsClasses) + biasesClasses
+            self.softmax = tf.reshape(tf.nn.softmax(flatten_logits),
+                                      (config.batch_size, -1,
+                                       config.num_classes))
 
-        flatten_logits = tf.matmul(flatten_outputs,
-                                   weightsClasses) + biasesClasses
-        self.softmax = tf.reshape(tf.nn.softmax(flatten_logits),
-                                  (config.batch_size, -1,
-                                   config.num_classes))
+            flatten_labels = tf.reshape(self.labels,
+                                        (-1, config.num_classes))
+            self.xent_loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(labels=flatten_labels,
+                                                        logits=flatten_logits))
 
-        flatten_labels = tf.reshape(self.labels,
-                                    (-1, config.num_classes))
-        self.xent_loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=flatten_labels,
-                                                    logits=flatten_logits))
+            # calculating maxpooling loss
+            self.log_softmax = -tf.log(self.softmax)
+            self.crop_log_softmax = tf.slice(self.log_softmax, [0, 0, 1],
+                                             [-1, -1, -1])
+            self.crop_labels = tf.slice(self.labels, [0, 0, 1], [-1, -1, -1])
+            self.masked_log_softmax = self.crop_log_softmax * self.crop_labels
+            self.segment_len = tf.count_nonzero(self.masked_log_softmax, 1,
+                                                dtype=tf.float32)  # shape (batchsize,class_num)
+            self.max_frame = tf.reduce_max(self.masked_log_softmax,
+                                           1)  # shape (batchsize,class_num)
+            self.xent_max_frame = tf.reduce_sum(
+                self.max_frame * self.segment_len)
+            self.background_log_softmax = tf.slice(self.log_softmax, [0, 0, 0],
+                                                   [-1, -1, 1])
+            self.background_label = tf.slice(self.labels, [0, 0, 0],
+                                             [-1, -1, 1])
+            self.xent_background = tf.reduce_sum(
+                tf.reduce_sum(
+                    self.background_log_softmax * self.background_label,
+                    (1, 2)) / tf.cast(self.seqLengths,
+                                      tf.float32))
 
-        # calculating maxpooling loss
-        self.log_softmax = -tf.log(self.softmax)
-        self.crop_log_softmax = tf.slice(self.log_softmax, [0, 0, 1],
-                                         [-1, -1, -1])
-        self.crop_labels = tf.slice(self.labels, [0, 0, 1], [-1, -1, -1])
-        self.masked_log_softmax = self.crop_log_softmax * self.crop_labels
-        self.segment_len = tf.count_nonzero(self.masked_log_softmax, 1,
-                                            dtype=tf.float32)  # shape (batchsize,class_num)
-        self.max_frame = tf.reduce_max(self.masked_log_softmax,
-                                       1)  # shape (batchsize,class_num)
-        self.xent_max_frame = tf.reduce_sum(
-            self.max_frame * self.segment_len)
-        self.background_log_softmax = tf.slice(self.log_softmax, [0, 0, 0],
-                                               [-1, -1, 1])
-        self.background_label = tf.slice(self.labels, [0, 0, 0],
-                                         [-1, -1, 1])
-        self.xent_background = tf.reduce_sum(
-            tf.reduce_sum(
-                self.background_log_softmax * self.background_label,
-                (1, 2)) / tf.cast(self.seqLengths,
-                                  tf.float32))
+            self.flatten_masked_softmax = tf.reshape(self.masked_log_softmax,
+                                                     (config.batch_size, -1))
+            self.max_index = tf.arg_max(self.flatten_masked_softmax, 1)
 
-        self.flatten_masked_softmax = tf.reshape(self.masked_log_softmax,
-                                                 (config.batch_size, -1))
-        self.max_index = tf.arg_max(self.flatten_masked_softmax, 1)
+            self.max_pooling_loss = self.xent_background + self.xent_max_frame
 
-        self.max_pooling_loss = self.xent_background + self.xent_max_frame
+            self.var_trainable_op = tf.trainable_variables()
 
-        self.var_trainable_op = tf.trainable_variables()
+            if config.max_pooling_loss:
+                self.loss = self.max_pooling_loss
+            else:
+                self.loss = self.xent_loss
 
-        if config.max_pooling_loss:
-            self.loss = self.max_pooling_loss
-        else:
-            self.loss = self.xent_loss
+            if config.grad_clip == -1:
+                # not apply gradient clipping
 
-        if config.grad_clip == -1:
-            # not apply gradient clipping
-
-            self.optimizer = tf.train.AdamOptimizer(
-                config.learning_rate).minimize(self.loss)
-        else:
-            # apply gradient clipping
-            grads, _ = tf.clip_by_global_norm(
-                tf.gradients(self.loss, self.var_trainable_op),
-                config.grad_clip)
-            opti = tf.train.AdamOptimizer(config.learning_rate)
-            self.optimizer = opti.apply_gradients(
-                zip(grads, self.var_trainable_op))
+                self.optimizer = tf.train.AdamOptimizer(
+                    config.learning_rate).minimize(self.loss)
+            else:
+                # apply gradient clipping
+                grads, _ = tf.clip_by_global_norm(
+                    tf.gradients(self.loss, self.var_trainable_op),
+                    config.grad_clip)
+                opti = tf.train.AdamOptimizer(config.learning_rate)
+                self.optimizer = opti.apply_gradients(
+                    zip(grads, self.var_trainable_op))
 
     def dropout(self, x, keep_prob):
         """ Apply dropout to a tensor
