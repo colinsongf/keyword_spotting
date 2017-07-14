0aa37712f1faa4ac7f735d6edc0f5d1988d9fbda
liuziqi <liuzq12@hotmail.com>
2017-07-14 18:43:19 +0800

    rm marker

--- marker/example.py
+++
@@ -1,6 +0,0 @@
-from pkg.pinyin_tone.pinyin import EngTonedMarker
-
-marker = EngTonedMarker()
-ret = marker.mark("你是谁hello")
-print(ret)
-assert(ret == ['ni3', 'shi4', 'shui2', 'h', 'e', 'l', 'l', 'o']) 
